31796326f53b997bcbbc9020aaacd1025cb19460
Sounish Nath <flock.sinasini@gmail.com>
2021-07-07 09:30:43 +0530

    Added Swagger / OpenAPI Configuration

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,6 +12,18 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
   app.setGlobalPrefix('api');
+
+  // OpenAPI Swagger Documentation
+  const configuration = new DocumentBuilder()
+    .setTitle('Twitter Backend API (Scaler)')
+    .setDescription('Demo of Building Scalable API with NestJS')
+    .setVersion('1.0')
+    .addTag('twitter')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, configuration);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(5000);
 }
 bootstrap();
