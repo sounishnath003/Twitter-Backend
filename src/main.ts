912dc3350229fadcc4b3de968321c4af96e014b2
Sounish Nath <flock.sinasini@gmail.com>
2021-07-07 09:25:11 +0530

    default server Fastify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   app.setGlobalPrefix('api');
   await app.listen(5000);
 }
